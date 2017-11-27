33d65d7a0498a31e533839f8f496faf9fc74bff5
yangtao <431910788@qq.com>
2017-11-27 21:23:26 +0800

    fix language

--- src/Controllers/MenuController.php
+++ src/Controllers/MenuController.php
@@ -25,8 +25,8 @@ class MenuController extends Controller
     public function index()
     {
         return Admin::content(function (Content $content) {
-            $content->header(trans('admin::lang.menu'));
-            $content->description(trans('admin::lang.list'));
+            $content->header(trans('admin.lang.menu'));
+            $content->description(trans('admin.:lang.list'));
 
             $content->row(function (Row $row) {
                 $row->column(6, $this->treeView()->render());
